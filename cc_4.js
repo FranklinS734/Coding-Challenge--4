6b672ad71037ed9466234029d6202e4f416a98ac
FranklinS734 <martinc3@usf.edu>
2025-01-31 21:56:20 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,14 @@ if (purchaseAmount > 100) {
     finalAmount = purchaseAmount * 0.85; // 15% discount
 }
 console.log(`Final amount after discount: $${finalAmount}`);
+
+// Task 2 - Sales Report
+// List of sales figures
+let sales = [230, 85, 320, 310, 60];
+let totalSales = 0;
+
+// Loop through sales array to add up total
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+console.log(`Total sales: $${totalSales}`);
